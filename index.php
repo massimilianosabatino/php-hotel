55d7af762f99b783bc38f3b66fb789f9ace74224
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-04-07 16:32:51 +0200

    add: bootstrap and table view

--- index.php
+++ index.php
@@ -48,34 +48,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <header>
         <h1>Hotels list</h1>
     </header>
     <main>
-        <ul>
-            <!-- Ciclo array -->
-            <?php foreach ($hotels as $hotel): ?>
-            <li>
-                <h2><?php echo $hotel['name'] ?></h2>
-                <p><?php echo $hotel['description'] ?></p>
-                <div>
+        <table class="table">
+            <thead>
+                <!-- Intestazione -->
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Hotel</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+                <!-- /Intestazione -->
+            </thead>
+            <tbody>
+                <!-- Singola riga -->
+                <!-- Ciclo array -->
+                <?php foreach ($hotels as $key => $hotel): ?>
+                <tr>
+                    <th scope="row"><?php echo $key + 1?></th>
+                    <td><?php echo $hotel['name'] ?></td>
+                    <td><?php echo $hotel['description'] ?></td>
                     <!-- Controllo disponibilità parcheggio -->
-                    <span>Parcheggio: </span>
                     <?php if($hotel['parking'] === true): ?>
-                    <span>Disponibile</span>
+                    <td>Disponibile</td>
                     <?php else: ?>
-                    <span>Non disponibile</span>
+                    <td>Non disponibile</td>
                     <?php endif; ?>
                     <!-- /Controllo disponibilità parcheggio -->
-                </div>
-                <div>Voto: <?php echo $hotel['vote'] ?></div>
-                <div>Distanza dal centro: <?php echo $hotel['distance_to_center']?></div>
-            </li>
-            <?php endforeach; ?>
-            <!-- /Ciclo array -->
-        </ul>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+                <!-- /Ciclo array -->
+                <!-- /Singola riga -->
+            </tbody>
+        </table>
     </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
